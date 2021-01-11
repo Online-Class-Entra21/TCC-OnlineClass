5a0b93af7b211ec3843a7585cbc8c96818cf30c5
AndreLuis-other <andreovidio55@gmail.com>
2021-01-11 13:37:53 -0300

    Revert "Update TestesAndre.java"
    This reverts commit 3bb53495431845c29b2e92fbdfb9969d5a39e97c.

--- backend/src/testes/TestesAndre.java
+++ backend/src/testes/TestesAndre.java
@@ -87,7 +87,42 @@ public class TestesAndre {
 		escolaDAO.insert(escola);
 		
 	}
-
+	
+	private static void testeUpdateEscola() {
+		int idEscola = 4;
+		String nome = "Escola Treis";
+		Date dataInicioLetivo = Date.valueOf("2020-02-10");
+		Date dataFinalLetivo = Date.valueOf("2020-12-10");
+		Escola escola = new Escola();
+		escola.setIdEscola(idEscola);
+		escola.setNome(nome);
+		escola.setDataInicioLeitvo(dataInicioLetivo);
+		escola.setDataFinalLetivo(dataFinalLetivo);
+		
+		EscolaDAO escolaDAO = new EscolaDAO();
+		
+		escolaDAO.update(escola);
+	}
+	
+	private static void testeDeleteEscola() {
+		EscolaDAO escolaDAO = new EscolaDAO();
+		escolaDAO.deleteID(2);
+		
+	}
+	private static void testeSelecionarIdEscola() {
+		EscolaDAO escolaDAO = new EscolaDAO();
+		Escola escola = escolaDAO.buscarId(1);
+		System.out.println(escola.toString());
+	}
+	private static void testeSelecionarTodosEscola() {
+		EscolaDAO escolaDAO = new EscolaDAO();
+		List<Escola> escolas = escolaDAO.buscarTodos();
+		
+		for (Escola escola : escolas) {
+			System.out.println(escola.toString());
+		}
+		
+	}
 	
 	
 }
