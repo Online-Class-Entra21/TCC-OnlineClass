e7b0bc56ef581206e84fcb1b3ea44e11b41bae4f
Breno Augusto Comandolli <brenoaugustocomandolli@gmail.com>
2021-01-15 14:35:27 -0300

    teste

--- backend/src/main/java/backend/api/CorsConfigure.java
+++ backend/src/main/java/backend/api/CorsConfigure.java
@@ -16,7 +16,7 @@ public class CorsConfigure {
 				registry.addMapping("/**")
 					.allowedMethods("GET","POST","PUT","DELETE")
 					.allowedHeaders("*")
-					.allowedOrigins("http://localhost:5500","http://127.0.0.1:5500");
+					.allowedOrigins("http://localhost:5500","http://127.0.0.1:5500","http://192.168.25.9:5500");
 			}
 		};
 	}
