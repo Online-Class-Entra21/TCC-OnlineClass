6d79325ece72d39139adfd1da941041a0e407908
Breno Augusto Comandolli <brenoaugustocomandolli@gmail.com>
2021-01-21 20:06:04 -0300

    Adicionando Classes Controller

---
+++ backend/src/main/java/backend/api/controller/reuniaoController.java
@@ -0,0 +1,13 @@
+package backend.api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Metodo controller da reuniao para consulta no banco de dados através da API Rest
+ * @author Breno
+ *
+ */
+@RestController
+public class reuniaoController {
+
+}
