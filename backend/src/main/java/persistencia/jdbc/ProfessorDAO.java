30192721553818ee8d356242d5fecb88b21988af
Breno Augusto Comandolli <brenoaugustocomandolli@gmail.com>
2021-01-23 12:48:09 -0300

    Alterando DAOs

---
+++ backend/src/main/java/persistencia/jdbc/ProfessorDAO.java
@@ -0,0 +1,56 @@
+package persistencia.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import entidade.Professor;
+
+/**
+ * Metodo para consulta do professor no banco de dados 
+ * @author Andrey
+ *
+ */
+public class ProfessorDAO {
+	
+	private Connection conexao = ConexaoFactory.getConnection();
+	
+	/**
+	 * Metodo para selecionar do banco de dados todos os professores cadastrados
+	 * @return lista de professores registrados no banco 
+	 * @author Andrey
+	 * @throws SQLException
+	 */
+	public List<Professor> buscarTodos() throws SQLException {
+		Professor professor = new Professor();
+		List<Professor> lista =  new ArrayList<Professor>();
+		String sql = "select * from usuario where tipoUsuario = 4";
+		
+		PreparedStatement comandoSql = conexao.prepareStatement(sql);
+		ResultSet resultSet = comandoSql.executeQuery();
+		
+		while (resultSet.next()) {
+			professor.setIdUsuario(resultSet.getInt(1));
+			professor.setNome(resultSet.getString(2));
+			professor.setSobrenome(resultSet.getString(3));
+			professor.setCpf(resultSet.getString(4));
+			professor.setTelefone(resultSet.getString(5));
+			professor.setCelular(resultSet.getString(6));
+			professor.setTipoUsuario(resultSet.getInt(7));
+			professor.setEmail(resultSet.getString(8));
+			professor.setSenha(resultSet.getString(9));
+			professor.setHorarioFinalExpediente(resultSet.getTime(10));
+			professor.setHorarioInicioExpediente(resultSet.getTime(11));
+			professor.setFotoUsuario(resultSet.getString(12));
+			professor.setFk_endereco(resultSet.getInt(13));
+			professor.setFk_escola(resultSet.getInt(14));
+			
+			lista.add(professor);
+		}
+		comandoSql.close();
+		return lista;
+	}
+}
--- backend/src/main/java/persistencia/jdbc/RespostaDAO.java
+++ backend/src/main/java/persistencia/jdbc/RespostaDAO.java
@@ -10,6 +10,11 @@ import java.util.List;
 
 import entidade.Resposta;
 
+/**
+ * Metodo para consulta da resposta no banco de dados 
+ * @author André
+ *
+ */
 public class RespostaDAO {
 
 	private Connection conexao = ConexaoFactory.getConnection();
@@ -21,33 +26,32 @@ public class RespostaDAO {
 	 * @throws SQLException 
 	 */	
 	public void insert(Resposta resposta) throws SQLException {
+		String sql = "insert into resposta (nota, comentarioatividade, correcaoatividade, dataentrega,"
+				   + " fk_aluno, fk_atividade) values (?, ?, ?, ?, ?, ?)";
+		PreparedStatement comandoSql = conexao.prepareStatement(sql);
 		
-			PreparedStatement comandoSql = conexao.prepareStatement("insert into resposta (nota, comentarioatividade, correcaoatividade, dataentrega, fk_aluno, fk_atividade) values (?, ?, ?, ?, ?, ?)");
-			
-			comandoSql.setDouble(1, resposta.getNota());
-			comandoSql.setString(2, resposta.getComentarioAtividade());
-			comandoSql.setBoolean(3, resposta.getCorrecaoAtividade());
-			comandoSql.setDate(4, (Date) resposta.getDataEntrega());
-			comandoSql.setInt(5, resposta.getFk_aluno());
-			comandoSql.setInt(6, resposta.getFk_atividade());
-			comandoSql.execute();
-			
-			comandoSql.close();
-			
+		comandoSql.setDouble(1, resposta.getNota());
+		comandoSql.setString(2, resposta.getComentarioAtividade());
+		comandoSql.setBoolean(3, resposta.getCorrecaoAtividade());
+		comandoSql.setDate(4, (Date) resposta.getDataEntrega());
+		comandoSql.setInt(5, resposta.getFk_aluno());
+		comandoSql.setInt(6, resposta.getFk_atividade());
+		comandoSql.execute();
+		
+		comandoSql.close();
 	}
 	
 	/**
-	 * Metodo para atualizar uma Resposta no banco de dados.
-	 * 
-	 * O id da <code>Resposta</code> deve ser o mesmo id que esta no banco de dados.
-	 * 
-	 * @param <code>Resposta</code>
-	 * @author Andre
+	 * Metodo para atualizar uma resposta no banco de dados.
+	 * O <code>idResposta</code> deve ser igual ao do resposta que deseja atualizar
+	 * @param Resposta resposta
+	 * @author André
 	 * @throws SQLException 
 	 */ 	
 	public void update(Resposta resposta) throws SQLException {
-		
-		PreparedStatement comandoSql = conexao.prepareStatement("update resposta set SET nota=?, comentarioatividade=?, correcaoatividade=?, dataentrega=?, fk_aluno=?, fk_atividade=? where idresposta = ?");
+		String sql = "update resposta set SET nota=?, comentarioatividade=?, correcaoatividade=?, dataentrega=?,"
+				   + " fk_aluno=?, fk_atividade=? where idresposta = ?";
+		PreparedStatement comandoSql = conexao.prepareStatement(sql);
 
 		comandoSql.setDouble(1, resposta.getNota());
 		comandoSql.setString(2, resposta.getComentarioAtividade());
@@ -58,48 +62,40 @@ public class RespostaDAO {
 		comandoSql.setInt(7, resposta.getIdResposta());
 		
 		comandoSql.execute();
-		
 		comandoSql.close();
-
 	}
 	
 	/**
-	 *  Metodo para deletar do banco de dados uma Resposta
-	 *  <p>
-	 *  O <code>idResposta</code> deve ser igual ao id do banco de dados
-	 * 
-	 * @param <code>Resposta</code>
-	 * @author Andre
+	 *  Metodo para deletar do banco de dados uma resposta.
+	 *  O <code>idResposta</code> deve ser igual ao do resposta que deseja deletar
+	 * @param int idResposta
+	 * @author André
 	 * @throws SQLException 
 	 */	
-	public void deleteId(int id) throws SQLException {
+	public void deleteId(int idResposta) throws SQLException {
+		String sql = "delete from resposta where idresposta = ?";
+		PreparedStatement comandoSql = conexao.prepareStatement(sql);
 		
-		PreparedStatement comandoSql = conexao.prepareStatement("delete from resposta where idresposta = ?");
+		comandoSql.setInt(1, idResposta);
 		
-		comandoSql.setInt(1, id);
 		comandoSql.execute();
-		
 		comandoSql.close();
-
 	}
 	
 	/**
-	 * Metodo para selecionar <code>Resposta</code> no banco de dados
-	 * <p>
-	 * O <code>idResposta</code> deve ser igual a Resposta que deseja selecionar
-	 * 
-	 * @param <code>idResposta<code>
-	 * @return Resposta
-	 * @author Andre
+	 * Metodo para selecionar a resposta no banco de dados.
+	 * O <code>idResposta</code> deve ser igual ao do resposta que deseja buscar
+	 * @param int idResposta
+	 * @return Resposta resposta
+	 * @author André
 	 * @throws SQLException 
 	 */	
 	public Resposta buscarId(int id) throws SQLException {
 		Resposta resposta = new Resposta();
-
-		PreparedStatement comandoSql = conexao.prepareStatement("select * from resposta where idresposta = ?");
+		String sql = "select * from resposta where idresposta = ?";
+		PreparedStatement comandoSql = conexao.prepareStatement(sql);
 		
-		comandoSql.setInt(1, id);
-					
+		comandoSql.setInt(1, id);	
 		ResultSet resultSet = comandoSql.executeQuery();
 		
 		if (resultSet.next()) {
@@ -110,32 +106,25 @@ public class RespostaDAO {
 			resposta.setDataEntrega(resultSet.getDate(5));
 			resposta.setFk_aluno(resultSet.getInt(6));
 			resposta.setFk_atividade(resultSet.getInt(7));
-			return resposta;
 		}
-		
 		comandoSql.close();
-	
-		
-		return null;
+		return resposta;
 	}
 	
 	/**
-	 * Metodo para selecionar todas as <code>Respostas</code> do banco de dados
-	 * 
-	 * @return <code>List</code>
-	 * @author Andre
+	 * Metodo para selecionar todas as respostas do banco de dados
+	 * @return lista de respostas registradas no banco
+	 * @author André
 	 * @throws SQLException 
 	 */	
 	public List<Resposta> buscarTodos() throws SQLException {
+		Resposta resposta = new Resposta();
 		List<Resposta> lista = new ArrayList<Resposta>();
-
 		PreparedStatement comandoSql = conexao.prepareStatement("select * from Resposta");
 		
 		ResultSet resultSet = comandoSql.executeQuery();
-		comandoSql.close();
 		
 		while (resultSet.next()) {
-			Resposta resposta = new Resposta();
 			resposta.setIdResposta(resultSet.getInt(1));
 			resposta.setNota(resultSet.getDouble(2));
 			resposta.setComentarioAtividade(resultSet.getString(3));
@@ -143,10 +132,10 @@ public class RespostaDAO {
 			resposta.setDataEntrega(resultSet.getDate(5));
 			resposta.setFk_aluno(resultSet.getInt(6));
 			resposta.setFk_atividade(resultSet.getInt(7));
+			
 			lista.add(resposta);
 		}
-		
+		comandoSql.close();
 		return lista;
 	}
-	
 }
