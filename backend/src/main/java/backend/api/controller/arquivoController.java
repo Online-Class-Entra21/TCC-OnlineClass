68675614cb9058291d5b288bdbc80ca160a87c68
andrey-romig <romig.andrey2017@gmail.com>
2021-01-23 12:37:24 -0300

    Adicionando Controllers e Diretor/CoordenadorDAO

---
+++ backend/src/main/java/backend/api/controller/arquivoController.java
@@ -0,0 +1,127 @@
+package backend.api.controller;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+
+import entidade.Aluno;
+import entidade.Arquivo;
+import persistencia.jdbc.AlunoDAO;
+import persistencia.jdbc.ArquivoDAO;
+
+/**
+ * Metodo controller do arquivo para consulta no banco de dados através da API Rest
+ * @author Breno
+ *
+ */
+@RestController
+public class ArquivoController {
+	
+	/**
+	 * Retorna o arquivo que corresponde ao id indicado {GET}
+	 * @param int codigo
+	 * @return String json
+	 */
+	@GetMapping(path = "/api/arquivo/{codigo}")
+	public String consultar(@PathVariable("codigo") int codigo) {
+		Arquivo arquivo;
+		ArquivoDAO arquivoDao = new ArquivoDAO();
+		try {
+			arquivo = arquivoDao.buscarId(codigo);
+			Gson gson = new Gson();
+			String json = gson.toJson(arquivo);
+			return json;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	/**
+	 * Retorna a lista de arquivos registrados no sistema {GET}
+	 * @return lista de arquivos registrados no banco 
+	 */
+	@GetMapping(path = "/api/arquivos")
+	public List<Arquivo> consultar(){
+		List<Arquivo> lista;
+		ArquivoDAO arquivoDao = new ArquivoDAO();
+		try {
+			lista = arquivoDao.buscarTodos();
+			return lista;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/**
+	 * Insere uma novo arquivo no banco de dados {POST}
+	 * @param String json
+	 */
+	@PostMapping(path = "api/arquivo/inserir/{json}")
+	public boolean inserir(@PathVariable("json") String json) {
+		Gson gson = new Gson();
+		Arquivo arquivo = gson.fromJson(json.toString(), Arquivo.class);
+		ArquivoDAO arquivoDao = new ArquivoDAO();
+		try {
+			arquivoDao.insert(arquivo);
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * Metodo para alteração do arquivo que corresponde ao codigo informado {PUT}
+	 * @param int codigo
+	 * @param String json
+	 */
+	@PutMapping(path = "api/arquivo/alterar/{codigo}/{json}")
+	public boolean alterar(@PathVariable("codigo") int codigo, @PathVariable("json") String json) {
+		Gson gson = new Gson();
+		Arquivo arquivo = gson.fromJson(json.toString(), Arquivo.class);
+		ArquivoDAO arquivoDao = new ArquivoDAO();
+		try {
+			arquivoDao.update(arquivo);
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	/**
+	 * Método de exclusão do arquivo que corresponde ao codigo informado {DELETE}
+	 * @param int codigo
+	 */
+	@DeleteMapping(path = "/api/arquivo/deletar/{codigo}")
+	public boolean deletar(@PathVariable("codigo") int codigo) {
+		ArquivoDAO arquivoDao = new ArquivoDAO();
+		try {
+			arquivoDao.deleteId(codigo);
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	//------------------------------------------------------------------
+	//Método Extras - Fora dos 5 principais 
+	//------------------------------------------------------------------
+}
---
+++ backend/src/main/java/reunioes/api/teste.java
@@ -0,0 +1,152 @@
+package reunioes.api;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.text.ParseException;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.microsoft.aad.msal4j.*;
+import com.nimbusds.jwt.JWTParser;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Controller exposing application endpoints
+ */
+@Controller
+public class teste {
+
+    @Autowired
+    AuthHelper authHelper;
+
+    @RequestMapping("/msal4jsample")
+    public String homepage(){
+        return "index";
+    }
+
+    @RequestMapping("/msal4jsample/secure/aad")
+    public ModelAndView securePage(HttpServletRequest httpRequest) throws ParseException {
+        ModelAndView mav = new ModelAndView("auth_page");
+
+        setAccountInfo(mav, httpRequest);
+
+        return mav;
+    }
+
+    @RequestMapping("/msal4jsample/sign_out")
+    public void signOut(HttpServletRequest httpRequest, HttpServletResponse response) throws IOException {
+
+        httpRequest.getSession().invalidate();
+
+        String endSessionEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/logout";
+
+        String redirectUrl = "http://localhost:8080/msal4jsample/";
+        response.sendRedirect(endSessionEndpoint + "?post_logout_redirect_uri=" +
+                URLEncoder.encode(redirectUrl, "UTF-8"));
+    }
+
+    @RequestMapping("/msal4jsample/graph/users")
+    public ModelAndView getUsersFromGraph(HttpServletRequest httpRequest, HttpServletResponse response)
+            throws Throwable {
+
+        IAuthenticationResult result;
+        ModelAndView mav;
+        try {
+            result = authHelper.getAuthResultBySilentFlow(httpRequest, response);
+        } catch (ExecutionException e) {
+            if (e.getCause() instanceof MsalInteractionRequiredException) {
+
+                // If silent call returns MsalInteractionRequired, then redirect to Authorization endpoint
+                // so user can consent to new scopes
+                String state = UUID.randomUUID().toString();
+                String nonce = UUID.randomUUID().toString();
+
+                SessionManagementHelper.storeStateAndNonceInSession(httpRequest.getSession(), state, nonce);
+
+                String authorizationCodeUrl = authHelper.getAuthorizationCodeUrl(
+                        httpRequest.getParameter("claims"),
+                        "User.ReadBasic.all",
+                        authHelper.getRedirectUriGraphUsers(),
+                        state,
+                        nonce);
+
+                return new ModelAndView("redirect:" + authorizationCodeUrl);
+            } else {
+
+                mav = new ModelAndView("error");
+                mav.addObject("error", e);
+                return mav;
+            }
+        }
+
+        if (result == null) {
+            mav = new ModelAndView("error");
+            mav.addObject("error", new Exception("AuthenticationResult not found in session."));
+        } else {
+            mav = new ModelAndView("auth_page");
+            setAccountInfo(mav, httpRequest);
+
+            try {
+                mav.addObject("users", getUserNamesFromGraph(result.accessToken()));
+
+                return mav;
+            } catch (Exception e) {
+                mav = new ModelAndView("error");
+                mav.addObject("error", e);
+            }
+        }
+        return mav;
+    }
+
+    private String getUserNamesFromGraph(String accessToken) throws Exception {
+        // Microsoft Graph users endpoint
+        URL url = new URL("https://graph.microsoft.com/v1.0/users");
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        // Set the appropriate header fields in the request header.
+        conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+        conn.setRequestProperty("Accept", "application/json");
+
+        String response = HttpClientHelper.getResponseStringFromConn(conn);
+
+        int responseCode = conn.getResponseCode();
+        if(responseCode != HttpURLConnection.HTTP_OK) {
+            throw new IOException(response);
+        }
+
+        JSONObject responseObject = HttpClientHelper.processResponse(responseCode, response);
+        JSONArray users = JSONHelper.fetchDirectoryObjectJSONArray(responseObject);
+
+        // Parse JSON to User objects, and append user names to string
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < users.length(); i++) {
+            JSONObject thisUserJSONObject = users.optJSONObject(i);
+            User user = new User();
+            JSONHelper.convertJSONObjectToDirectoryObject(thisUserJSONObject, user);
+            builder.append(user.getUserPrincipalName());
+            builder.append("<br/>");
+        }
+        return builder.toString();
+    }
+
+    private void setAccountInfo(ModelAndView model, HttpServletRequest httpRequest) throws ParseException {
+        IAuthenticationResult auth = SessionManagementHelper.getAuthSessionObject(httpRequest);
+
+        String tenantId = JWTParser.parse(auth.idToken()).getJWTClaimsSet().getStringClaim("tid");
+
+        model.addObject("tenantId", tenantId);
+        model.addObject("account", SessionManagementHelper.getAuthSessionObject(httpRequest).account());
+    }
+}
