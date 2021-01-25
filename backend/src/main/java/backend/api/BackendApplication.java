ecf484f1e226525853e44ae59cab1f127ebe0577
AndreLuis-other <andreovidio55@gmail.com>
2021-01-25 20:22:39 -0300

    adicionando logback.xml

--- backend/src/main/java/backend/api/BackendApplication.java
+++ backend/src/main/java/backend/api/BackendApplication.java
@@ -1,5 +1,7 @@
 package backend.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,8 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class BackendApplication {
-
+	public static final Logger LOGGER = LoggerFactory.getLogger(BackendApplication.class);  
 	public static void main(String[] args) {
+		LOGGER.info("Teste Erro");
 		SpringApplication.run(BackendApplication.class, args);
 	}
 }
