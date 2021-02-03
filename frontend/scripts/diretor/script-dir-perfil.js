91a5d2984940b152810841a1497dc70645c9b1f2
Ian Vicenzo <ivicenzo71@gmail.com>
2021-02-02 23:06:02 -0300

    Tela Diretor Página Cadastro Coordenador
    é isso ai

--- frontend/scripts/diretor/script-dir-perfil.js
+++ frontend/scripts/diretor/script-dir-perfil.js
@@ -106,3 +106,24 @@ $().ready(function() {
 		ImagePreview(this);
 	});
 });
+
+const imgs = document.getElementsByTagName('img');
+
+function abrirImg() {
+    for (var i = 0; i<imgs.length; i++) {
+        if(imgs[i].style.display == 'none' || imgs[i].style.display == '') {
+          imgs[i].style.display = 'inline-block';
+          break;
+        }
+      }
+}
+
+function apagar() {
+    for (var i = imgs.length - 1; i>=0; i--) {
+      if(imgs[i].style.display == 'inline-block') {
+        imgs[i].style.display = 'none';
+        break;
+      }
+    }
+  }
+  
