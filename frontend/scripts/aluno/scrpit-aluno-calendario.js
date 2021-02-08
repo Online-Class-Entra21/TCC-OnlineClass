d8919cda3a5db3a1769413a208227ad5c92dbe13
Ian Vicenzo <ivicenzo71@gmail.com>
2021-02-08 20:12:18 -0300

    Ta legal andré

---
+++ frontend/scripts/aluno/scrpit-aluno-calendario.js
@@ -0,0 +1,47 @@
+// Pegando id do usuário que logou 
+var idUsuario = sessionStorage.getItem("idUsuario");
+
+//Verifica se o idUsuario é válido 
+if(idUsuario != 0 && idUsuario != null){
+    //Busca dos dados do usuário
+    var xhr = new XMLHttpRequest(); 
+
+        xhr.open("GET", "http://localhost:8080/api/usuario/"+idUsuario);
+
+        xhr.addEventListener("load", function(){
+            var resposta = xhr.responseText; 
+            dadosUsuario = JSON.parse(resposta);
+            //Adiciona o nome 
+            document.getElementById("idNomeUsuario").textContent = dadosUsuario.nome;
+            //Adiciona a foto de perfil do usuario
+            var img = document.querySelector("#idFotoPerfil");
+            img.setAttribute('src', dadosUsuario.fotoUsuario);
+            img.style.borderRadius = "80%";
+        })
+
+    xhr.send();
+    
+}else{
+    // alert('Sessão expirada - Erro (0002)')
+    // window.location = "/frontend/index.html";
+}
+
+//Evento de abertura do menu 
+document.getElementById("mostrar").addEventListener("mouseover", function(){
+    abrirMenu();
+})
+document.getElementById("idImgMenu").addEventListener("mouseover", function(){
+    abrirMenu();
+})
+
+//Abertura do menu
+function abrirMenu(){
+    document.getElementById("menu").style.display = "block";
+}
+
+//Evento de fechamento do menu 
+document.getElementById("menu").addEventListener("mouseleave", function(){
+    document.getElementById("menu").style.display = "none";
+})
+
+
