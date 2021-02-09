a1b30641208095635556d0ef1109e5f5e06e0848
Breno Augusto Comandolli <brenoaugustocomandolli@gmail.com>
2021-02-09 16:28:22 -0300

    ....

--- frontend/scripts/aluno/script-aluno-principal.js
+++ frontend/scripts/aluno/script-aluno-principal.js
@@ -10,13 +10,15 @@ if(idUsuario != 0 && idUsuario != null){
 
         xhr.addEventListener("load", function(){
             var resposta = xhr.responseText; 
-            dadosUsuario = JSON.parse(resposta);
+            var dadosUsuario = JSON.parse(resposta);
             //Adiciona o nome 
-            document.getElementById("idNomeUsuario").textContent = dadosUsuario.nome;
+            document.getElementById("idNomeUsuario").textContent = dadosUsuario.nome +" "+dadosUsuario.sobrenome;
             //Adiciona a foto de perfil do usuario
             var img = document.querySelector("#idFotoPerfil");
-            img.setAttribute('src', dadosUsuario.fotoUsuario);
-            img.style.borderRadius = "80%";
+            if(dadosUsuario.fotoUsuario != null){
+                img.setAttribute('src', "/imagens-usuarios/"+dadosUsuario.fotoUsuario);
+                img.style.borderRadius = "80%";
+            }
         })
 
     xhr.send();
@@ -25,25 +27,6 @@ if(idUsuario != 0 && idUsuario != null){
     // alert('Sessão expirada - Erro (0002)')
     // window.location = "/frontend/index.html";
 }
-
-//Evento de abertura do menu 
-document.getElementById("mostrar").addEventListener("mouseover", function(){
-    abrirMenu();
-})
-document.getElementById("idImgMenu").addEventListener("mouseover", function(){
-    abrirMenu();
-})
-
-//Abertura do menu
-function abrirMenu(){
-    document.getElementById("menu").style.display = "block";
-}
-
-//Evento de fechamento do menu 
-document.getElementById("menu").addEventListener("mouseleave", function(){
-    document.getElementById("menu").style.display = "none";
-})
-
 
 //Abre uma nova guia para digitar o email de recuperação 
 function answer() {
