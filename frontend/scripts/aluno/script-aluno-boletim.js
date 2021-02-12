89353cd2441a71bc20e7945a5d049ac4e5ae02b9
Ian Vicenzo <ivicenzo71@gmail.com>
2021-02-11 22:24:57 -0300

    GerarBoletim

--- frontend/scripts/aluno/script-aluno-boletim.js
+++ frontend/scripts/aluno/script-aluno-boletim.js
@@ -1,5 +1,5 @@
-function CriaPDF() {
-    var meuBoletim = document.getElementById('idBoletim').innerHTML;
+function gerarBoletim() {
+    var notas = document.getElementById('idBoletim').innerHTML;
     var style = "<style>";
     style = style + "table#t {width: 100%;font: 20px Calibri;}";
     style = style + "#t {border: solid 1px #DDD; border-collapse: collapse;";
@@ -7,15 +7,15 @@ function CriaPDF() {
     style = style + "table#i {witdh: 100%}; font-size:20px; margin:5%; padding: 2px 3px;";
     style = style + "#i {border: 2px solid gray; border-collapse: collapse; text-align: center;}";
     style = style + "</style>";
-    // CRIA UM OBJETO WINDOW
+    
     var win = window.open('', '', 'height=700,width=700');
     win.document.write('<html><head>');
-    win.document.write('<title>Boletim</title>');   // <title> CABEÇALHO DO PDF.
-    win.document.write(style);                                     // INCLUI UM ESTILO NA TAB HEAD
+    win.document.write('<title>Boletim</title>');
+    win.document.write(style);
     win.document.write('</head>');
     win.document.write('<body>');
-    win.document.write(meuBoletim);                          // O CONTEUDO DA TABELA DENTRO DA TAG BODY
+    win.document.write(notas);
     win.document.write('</body></html>');
-    win.document.close(); 	                                         // FECHA A JANELA
-    win.print();                                                            // IMPRIME O CONTEUDO
+    win.document.close();
+    win.print();
 }
