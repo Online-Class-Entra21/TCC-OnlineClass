a7ee4d90bcc4e4ccf138b83df08ee3f3e071c2b2
andrey-romig <romig.andrey2017@gmail.com>
2021-02-13 23:57:57 -0300

    update aluno-resposta

--- frontend/scripts/aluno/script-aluno-resposta.js
+++ frontend/scripts/aluno/script-aluno-resposta.js
@@ -1,3 +1,30 @@
+//Método para chamada da API async
+function usarApi(method, url) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
+}
+
+
+
 //Pega o id da atividade e o aluno
 var idAtividade = sessionStorage.getItem('idAtividadeEscolhida')
 var idAluno = sessionStorage.getItem('idAluno');
@@ -15,12 +42,12 @@ async function carregarTitulo() {
     var respostaExistente = JSON.parse(resposta);
     if (respostaExistente != null) {
         document.getElementById('area').value = respostaExistente.comentarioAtividade;
-        var confirmar = confirm("Você ja respondeu essa atividade, responder novamente?\nAo clicar em OK, a resposta enviada anteriormente será deletada.")
+        var confirmar = confirm("Você ja respondeu essa atividade, responder novamente?\nAo clicar em OK, a resposta enviada anteriormente será removida.")
         if (confirmar == false) {
             window.close();
         } else{
             //Exclui a resposta atual
-
+            //await usarApi("DELETE", "http://localhost:8080/api/resposta/deletar/" + respostaExistente.idResposta);
         }
     } 
 }
@@ -56,7 +83,7 @@ async function enviar() {
         fk_atividade: idAtividade,
         fk_arquivo: 16
     }
-    
+    console.log(resposta);
     var insertResposta = JSON.stringify(resposta);
     console.log(insertResposta)
     var resposta = await usarApi("POST", "http://localhost:8080/api/resposta/inserir/" + insertResposta);
