6cab87353ce3edbd34e4567ee399a004cc731217
Breno Augusto Comandolli <brenoaugustocomandolli@gmail.com>
2021-02-19 11:31:49 -0300

    ...

--- backend/src/main/java/persistencia/jdbc/ConexaoFactory.java
+++ backend/src/main/java/persistencia/jdbc/ConexaoFactory.java
@@ -17,7 +17,7 @@ public class ConexaoFactory {
 			Class.forName("org.postgresql.Driver");
 			
 			if (conexao == null) {
-				conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/OnlineClassBD", "postgres","33367743");
+				conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/OnlineClassBD", "postgres","adm123");
 			}
 			return conexao;
 		} catch (Exception e) {
